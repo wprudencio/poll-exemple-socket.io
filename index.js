bde4eb421f31f5d35b294da366a3e9bba89e4ec3
Weslei Prudencio <wesleiprudencio@gmail.com>
2016-01-24 20:16:52 -0300

    instalacao c3 (biblioteca de graficos)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var express = require('express');
 var http = require('http').Server(app);
 
-app.use(express.static('static'));
+app.use(express.static('public'));
 
 var PORT = 3000;
 
@@ -10,6 +10,7 @@ http.listen(PORT, function() {
     console.log('listening on *:3000');
 });
 
+// Home
 app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
---
+++ public/app.js
@@ -0,0 +1,15 @@
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        columns: [
+            ['Narcos', 0],
+            ['Breaking Bad', 0],
+        ],
+        type: 'bar'
+    }
+});
+
+var nc = 10;
+var bb = 10;
+
+
